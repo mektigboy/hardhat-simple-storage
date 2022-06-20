b5d0f73862719babcb3df8bace7d40354c0e54ac
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-20 14:48:31 -0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,16 @@
-
+const { ethers } = require("ethers");
 
 async function main() {
+	const SimpleStorageFactory = await ethers.getContractFactory(
+		"SimpleStorage"
+	);
 
-}
+	console.log("Deploying contract...");
+
+	const simpleStorage = await SimpleStorageFactory.deploy();
+	
+	await simpleStorage.deployed();
+};
 
 main()
 	.then(() => process.exit(0))
