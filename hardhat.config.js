b968f02c2a426e00ebb389da68513c01c16883b4
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-21 15:28:47 -0500

    Update scripts/deploy.js, hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  };
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
@@ -15,16 +15,16 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 4
-    }
-  },
-  solidity: "0.8.15",
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY
-  }
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+    },
+    solidity: "0.8.15",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,44 +1,44 @@
 const { ethers, run, network } = require("hardhat");
 
 async function main() {
-	const SimpleStorageFactory = await ethers.getContractFactory(
-		"SimpleStorage"
-	);
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    );
 
-	console.log("Deploying contract...");
+    console.log("Deploying contract...");
 
-	const simpleStorage = await SimpleStorageFactory.deploy();
-	
-	await simpleStorage.deployed();
+    const simpleStorage = await SimpleStorageFactory.deploy();
 
-	console.log(`Deployed contract to: ${simpleStorage.address}`);
-	
-	if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
-		await simpleStorage.deployTransaction.wait(6);
-		await verify(simpleStorage.address, []);
-	}
-};
+    await simpleStorage.deployed();
+
+    console.log(`Deployed contract to: ${simpleStorage.address}`);
+
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    }
+}
 
 async function verify(contractAddress, args) {
-	console.log("Veryfing contract...");
-	
-	try {
-		await run("verify:verify", {
-			address: contractAddress,
-			contractArguments: args
-		});
-	} catch (e) {
-		if (e.message.toLowerCase().includes("already verified")){
-			console.log("Already verified!");
-		} else {
-			console.log(e);
-		};
-	};
-};
+    console.log("Veryfing contract...");
+
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            contractArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!");
+        } else {
+            console.log(e);
+        }
+    }
+}
 
 main()
-	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error(error);
-		process.exit(1);
-	});
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
