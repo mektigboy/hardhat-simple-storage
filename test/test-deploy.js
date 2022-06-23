4c298a2bbe57562f02d79731fb354239fa159171
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-22 20:35:09 -0500

    Update test/test-deploy.js

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -1,3 +1,17 @@
-describe("SimpleStorage", function() {
+const { ethers } = require("hardhat");
+const { expect, assert } = require("chai");
 
+describe("SimpleStorage", function () {
+    let simpleStorageFactory, simpleStorage;
+
+    beforeEach(async function () {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+        simpleStorage = await simpleStorageFactory.deploy();
+    });
+
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve();
+        const expectedValue = "0";
+        assert.equal(currentValue.toString(), expectedValue);
+    });
 });
