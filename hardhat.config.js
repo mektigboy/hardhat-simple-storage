d207e4eeaceacd62b9ffc22b0fe49a28e25cb2a5
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-22 21:49:29 -0500

    Update hardhat.config.js | Yarn add solidity-coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
 require("hardhat-gas-reporter");
+require("solidity-coverage")
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
@@ -12,10 +13,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
 });
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 module.exports = {
     defaultNetwork: "hardhat",
