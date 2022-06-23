5006d5c1078b3d0ec88c7aa5f13c3ffe1aa53cf1
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-22 20:48:54 -0500

    Update test/test-deploy.js

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -9,9 +9,17 @@ describe("SimpleStorage", function () {
         simpleStorage = await simpleStorageFactory.deploy();
     });
 
-    it("Should start with a favorite number of 0", async function () {
+    it("Should start with a favorite number of: 0.", async function () {
         const currentValue = await simpleStorage.retrieve();
         const expectedValue = "0";
+        assert.equal(currentValue.toString(), expectedValue);
+    });
+
+    it("Should update when we call store.", async function () {
+        const expectedValue = "7";
+        const transactionResponse = await simpleStorage.store(expectedValue);
+        await transactionResponse.wait(1);
+        const currentValue = await simpleStorage.retrieve();
         assert.equal(currentValue.toString(), expectedValue);
     });
 });
