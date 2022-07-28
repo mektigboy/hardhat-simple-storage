58a36bc0c5aacda60dde9b47323422c6c686b4d5
mektigboy <mektigboy@protonmail.com>
2022-07-28 07:20:18 -0500

    Update contracts/SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,26 +2,27 @@
 pragma solidity ^0.8.15;
 
 contract SimpleStorage {
+    // This gets initialized to its default value.
     uint256 favoriteNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     struct People {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
-    People[] public people;
 
-    mapping(string => uint256) public nameToFavoriteNumber;
+    People[] public people;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
-    function retrieve() public view returns (uint256) {
+    function retrieve() public view returns(uint256) {
         return favoriteNumber;
     }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
